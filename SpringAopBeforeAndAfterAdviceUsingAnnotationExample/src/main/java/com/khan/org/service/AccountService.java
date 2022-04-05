c6763f0ad38c106b9d286b3c9504619b4595dbfc
Zaid Khan <zaidkhan5548@gmail.com>
2022-04-05 14:28:23 +0530

    Revert "Spring 5 Core Practice projects"
    This reverts commit d0383398dd065719579d82a077cf24db29309f04.

--- SpringAopBeforeAndAfterAdviceUsingAnnotationExample/src/main/java/com/khan/org/service/AccountService.java
+++
@@ -1,7 +0,0 @@
-package com.khan.org.service;
-
-import com.khan.org.model.Account;
-
-public interface AccountService {
-	public abstract void updateAccountBalance(Account account,Long amount);
-}
--- SpringDIWithFactoryMethod/src/main/java/com/khan/model/Printer.java
+++
@@ -1,7 +0,0 @@
-package com.khan.model;
-
-public class Printer {
-	public void printBalanceInformation(String accountNumber) {
-		System.out.println("The Printer has print the balance information of the account "+accountNumber);
-	}
-}
